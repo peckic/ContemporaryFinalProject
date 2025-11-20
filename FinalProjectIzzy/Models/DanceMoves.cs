ff21549d341a659ed69b6c7bc40b6045a020516d
peckic <peckic@mail.uc.edu>
2025-11-20 13:53:53 -0500

    Add project files.

---
+++ FinalProjectIzzy/Models/DanceMoves.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace FinalProjectIzzy.Models
+{
+    public class DanceMoves
+    {
+        public string MoveNameId { get; set; }
+        public string Style { get; set; }
+        public int Difficulty { get; set; }
+        public string MusicType { get; set; }
+        public int IdealTempo { get; set; }
+        public DanceMoves() { }
+        public DanceMoves(string moveNameId, string style, int difficulty, string musicType, int idealTempo)
+        {
+            MoveNameId = moveNameId;
+            Style = style;
+            Difficulty = difficulty;
+            MusicType = musicType;
+            IdealTempo = idealTempo;
+        }
+    }
+}
