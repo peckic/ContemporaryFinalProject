0bce899b78aa6d7a44293472f1ed71a441c87af2
Zinse2 <ethan.zins@yahoo.com>
2025-11-25 13:44:08 -0500

    Update Nature

--- FinalProjectIzzy/Program.cs
+++ FinalProjectIzzy/Program.cs
@@ -1,23 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using FinalProjectIzzy.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+// register services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+// enable Swagger UI at root: https://localhost:5001/
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseExceptionHandler("/Home/Error");
-}
-app.UseStaticFiles();
-
-app.UseRouting();
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+    c.RoutePrefix = string.Empty; // serve UI at "/"
+});
 
 app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.MapControllers();
 app.Run();
